b3d4238d92f0402573fab8a644ce51b78fdf73b0
ZhiZYY <601903495@qq.com>
2025-10-04 17:11:32 +0800

    前端和其他文件的初始结构

---
+++ leadingEnd/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <LoginPage />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    LoginPage: () => import('./components/LoginPage.vue')
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  width: 100%;
+}
+</style>

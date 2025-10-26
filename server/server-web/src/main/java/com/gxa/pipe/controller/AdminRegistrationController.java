938d3bd1b7fa9d65cb2522cd3a30e3db06be578f
ZhiZYY <601903495@qq.com>
2025-10-26 15:55:06 +0800

    refactor(controller): 移除AdminRegistrationController中无用的RequestMapping注解

--- server/server-web/src/main/java/com/gxa/pipe/controller/AdminRegistrationController.java
+++ server/server-web/src/main/java/com/gxa/pipe/controller/AdminRegistrationController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gxa.pipe.pojo.dto.request.AdminLoginRequest;
